d4197b8df53de508c685e473f02784e18a92f73e
Melroy Krebbekx <0982689@hr.nl>
2022-10-11 13:52:20 +0200

    Added threading and data convs

---
+++ AutonomeCar/program.py
@@ -0,0 +1,20 @@
+from serial_port import SerialPort
+from threading import Thread
+
+
+class ReadAndProcess(Thread):
+    def __init__(self, serial_class):
+        Thread.__init__(self)
+        self.serial_class = serial_class
+
+    def run(self):
+        while True:
+            self.serial_class.read_data()
+            print(self.serial_class.return_processed_array())
+
+
+if __name__ == "__main__":
+    serial_arduino = SerialPort(9600)
+    read_and_process_thread = ReadAndProcess(serial_class=serial_arduino)
+    read_and_process_thread.start()
+    read_and_process_thread.join()
---
+++ AutonomeCar/serial_port.py
@@ -0,0 +1,47 @@
+import serial
+import serial.tools.list_ports
+from typing import Union, List
+
+
+# 150 degrees : 10 = 15 angles. array 14
+
+def find_open_port():
+    ports = list(serial.tools.list_ports.comports())
+    for port in ports:
+        return port[0]
+
+
+class SerialPort:
+    def __init__(self, baud):
+        port = find_open_port()
+        self.processed_array = [None] * 15
+        self.open_port = serial.Serial(port, baud, timeout=.1)
+        print("connected to: " + self.open_port.portstr)
+
+    def send_data(self, data):
+        number = self.open_port.write(data)
+        return number
+
+    def read_data(self):
+        if self.open_port.in_waiting > 0:
+            unprocessed_data = str(self.open_port.readline()[:-2], 'utf-8')
+        else:
+            return
+
+        temp_data = unprocessed_data.split(",")
+        for idx, string_int in enumerate(temp_data):
+            temp_data[idx] = int(string_int)
+
+        self.processed_array = temp_data
+
+    def return_processed_array(self) -> Union[List[None], List[int]]:
+        """
+        Returns processed array
+        """
+        return self.processed_array
+
+
+if __name__ == "__main__":
+    serial = SerialPort(9600)
+    while True:
+        print(serial.read_data())
