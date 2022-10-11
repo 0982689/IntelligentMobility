f5c71f5aa5db01e68ce70d3d09ac289820a56e12
GaurishArkesteyn <gaurish.ark@gmail.com>
2022-10-11 10:36:06 +0200

    added serial communication v1

---
+++ AutonomeCar/test.py
@@ -0,0 +1,15 @@
+import serialPort,threading,time
+import serial.tools.list_ports
+
+ports = list(serial.tools.list_ports.comports())
+for p in ports:
+    print(p)
+
+arduino = serialPort.serialPort(p[0],9600)
+t1 = threading.Thread(target=arduino.read_data)
+t1.start()
+while True:
+    time.sleep(1)
+    print(arduino.message)
+
+
