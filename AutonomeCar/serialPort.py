5d6de2e8e5faf6a39a99627392e40a54d034e681
Melroy Krebbekx <0982689@hr.nl>
2022-10-11 10:47:48 +0200

    removed useless shit

--- AutonomeCar/serialPort.py
+++ AutonomeCar/serialPort.py
@@ -1,29 +1,34 @@
 import serial
 
-class serialPort:
-	message=''
-	def __init__(self,port,baud):
-		self.port=serial.Serial(port,baud,timeout=.1)
-		print("connected to: " + self.port.portstr)
-		if not self.port.isOpen():
-			self.port.open()
-			print("connected to: " + self.port.portstr)
 
-	def port_open(self):
-		if not self.port.isOpen():
-			self.port.open()
-			print("connected to: " + self.port.portstr)
+class SerialPort:
+    message = ''
 
-	def port_close(self):
-		self.port.close()
+    def __init__(self, baud):
+        ports = list(serial.tools.list_ports.comports())
+        for p in ports:
+            print(p)
+        self.port = serial.Serial(port, baud, timeout=.1)
+        print("connected to: " + self.port.portstr)
+        if not self.port.isOpen():
+            self.port.open()
+            print("connected to: " + self.port.portstr)
 
-	def send_data(self,data):
-		number=self.port.write(data)
-		return number
+    def port_open(self):
+        if not self.port.isOpen():
+            self.port.open()
+            print("connected to: " + self.port.portstr)
 
-	def read_data(self):
-			if (self.port.in_waiting>0):
-				data=self.port.readline()[:-2]
-				decodedData = str(data,'utf-8')
-				# print(decodedData)
-				self.message+=decodedData
+    def port_close(self):
+        self.port.close()
+
+    def send_data(self, data):
+        number = self.port.write(data)
+        return number
+
+    def read_data(self):
+        if self.port.in_waiting > 0:
+            data = self.port.readline()[:-2]
+            decoded_data = str(data, 'utf-8')
+            # print(decodedData)
+            self.message = decodedData
