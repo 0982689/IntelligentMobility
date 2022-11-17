fe816f3e8e2df9c5d17c20e98186fb0d3b4f3baa
Steven Ren <0979514@hr.nl>
2022-11-17 13:39:25 +0100

    Added comments for clarity on test file

--- Car/unit_tests/test_program.py
+++ Car/unit_tests/test_program.py
@@ -8,13 +8,24 @@ read_and_process_thread = ReadAndProcess(serial_class=serialPort)
 class TestProgram(TestCase):
     
     def setUp(self) -> None:
+        """
+        Starting read and process thread for tests.
+        """
         read_and_process_thread.start()
         read_and_process_thread.join()
     
-    def test_processed_array_not_none(self):
+    def test_processed_array_not_none(self) -> None:
+        """
+        Test will fail if None is in processed array which means
+        that we do not get Lidar info
+        """
         self.assertNotIn(None, serialPort.processed_array)
 
-    def test_processed_array_none(self):
+    def test_processed_array_none(self) -> None:
+        """
+        Test will succeed if None is in processed array which means
+        that we do not get Lidar info
+        """
         self.assertIn(None, serialPort.processed_array)
           
 if __name__ == "__main__":
