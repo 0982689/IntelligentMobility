1f16df4fdc4148165b5acb70127f59ec6d9d2ea4
Audrynass <audrius@live.nl>
2022-12-13 13:55:18 +0100

    merge fix

---
+++ simpylc_lib/simpylc/simulations/car/program.py
@@ -0,0 +1,28 @@
+from threading import Thread
+from simpylc_lib.simpylc.simulations.car.clients import RLClient
+
+
+# Make threaded class for reading the arduino via serial
+class ReadAndProcess(Thread):
+    def __init__(self, serial_class):
+        Thread.__init__(self)
+        self.serial_class = serial_class
+
+    # Standard run class from package thread which prints the data from the aruino
+    def run(self):
+        while True:
+            self.serial_class.read_data()
+            print(self.serial_class.return_processed_array())
+
+
+# Main starts program to read serial
+if __name__ == "__main__":
+    # serial_arduino = SerialPort(9600)
+    # read_and_process_thread = ReadAndProcess(serial_class=serial_arduino)
+    # read_and_process_thread.start()
+    # read_and_process_thread.join()
+    client = RLClient()
+    client.servo.set_pulse(10)
+    # client = AIClient()
+    # client.use_sim()
+    # client.plot_loss()
---
+++ simpylc_lib/simpylc/simulations/car/unit_tests/test_program.py
@@ -0,0 +1,41 @@
+from unittest import TestCase, main
+from simpylc_lib.simpylc.simulations.car.program import ReadAndProcess
+from simpylc_lib.simpylc.simulations.car.serial_port import SerialPort
+
+serial_port = SerialPort(9600)
+connected = serial_port.open_port.is_open
+
+"""
+Run with pytest
+"""
+class TestProgram(TestCase):
+
+    def setUp(self) -> None:
+        """
+        Instantiating the SerialPort class and 
+        starting the read and process thread for tests.
+        """
+        if not connected:
+            # Skips all the tests inside this class
+            self.skipTest("Could not connect to comport.")
+        read_and_process_thread = ReadAndProcess(serial_class=serial_port)
+        read_and_process_thread.start()
+        read_and_process_thread.join()
+
+    def test_processed_array_not_none(self) -> None:
+        """
+        Test will fail if None is in processed array, which means
+        that we do not get Lidar info
+        """
+        self.assertNotIn(None, serial_port.processed_array)
+
+    def test_processed_array_none(self) -> None:
+        """
+        Test will succeed if None is in processed array, which means
+        that we do not get Lidar info
+        """
+        self.assertIn(None, serial_port.processed_array)
+
+
+if __name__ == "__main__":
+    main()
