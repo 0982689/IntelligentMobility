a23c68f99e541e67abc479ce14c6d61bd5056d53
Melroy Krebbekx <0982689@hr.nl>
2022-12-15 12:31:47 +0100

    testing motors

--- simpylc_lib/simpylc/simulations/car/program.py
+++ simpylc_lib/simpylc/simulations/car/program.py
@@ -15,6 +15,7 @@ class ReadAndProcess(Thread):
             print(self.serial_class.return_processed_array())
 
 
+# 40, 170, 400
 # Main starts program to read serial
 if __name__ == "__main__":
     # serial_arduino = SerialPort(9600)
@@ -22,6 +23,6 @@ if __name__ == "__main__":
     # read_and_process_thread.start()
     # read_and_process_thread.join()
     client = RLClient()
-    client.servo.set_pulse(40)
+    client.servo.set_pulse(160)
     # client = AIClient()
     # client.use_sim()
