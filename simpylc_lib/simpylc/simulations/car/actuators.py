67f4d02ab493b085e004c9e8d928feb9cd0e384d
Melroy Krebbekx <0982689@hr.nl>
2022-12-15 12:41:51 +0100

    testing motors

--- simpylc_lib/simpylc/simulations/car/actuators.py
+++ simpylc_lib/simpylc/simulations/car/actuators.py
@@ -36,7 +36,7 @@ class PWMServo(_PCA9685):
 
 
 class PWMMotors(_PCA9685):
-    def __init__(self, channel: int = 6) -> None:
+    def __init__(self, channel: int = 7) -> None:
         super().__init__(channel=channel)
 
 
