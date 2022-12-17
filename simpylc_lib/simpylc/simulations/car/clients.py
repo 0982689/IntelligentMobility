bbc611541e465654b9dc2a9a2d63ae7b36359e31
Audrynass <audrius@live.nl>
2022-12-17 18:24:54 +0100

    Improved model using tuning

--- simpylc_lib/simpylc/simulations/car/clients.py
+++ simpylc_lib/simpylc/simulations/car/clients.py
@@ -9,6 +9,7 @@ import socket_wrapper as sw
 from sklearn.neural_network import MLPRegressor
 from sklearn.preprocessing import MinMaxScaler
 import matplotlib.pyplot as plt
+from sklearn.model_selection import GridSearchCV, cross_val_score, cross_validate
 
 ss.path += [os.path.abspath(relPath) for relPath in ('..',)]
 
@@ -18,17 +19,20 @@ lidar_input_dim = 15
 
 sampleFileName = 'default.samples'
 
-X = np.loadtxt("default.samples", delimiter=' ')
+samples = np.loadtxt("default.samples", delimiter=' ')
+X = samples[:, :-1]
+Y = samples[:, -1]
 
-modelSaveFile = 'model.sav'
+modelSaveFile = 'tempmodel.sav'#'model.sav'
 
 
 def getTargetVelocity(steeringAngle) -> float:
     return (90 - abs(steeringAngle)) / 60
 
 def normalize_data() -> None:
+    global X
     scaler = MinMaxScaler()
-    X[:, :-1] = scaler.fit_transform(X[:, :-1])
+    X = scaler.fit_transform(X)
 
 class AIClient:
     def __init__(self) -> None:
@@ -37,15 +41,16 @@ class AIClient:
     def train_network(self) -> None:
         print("Training...")
         normalize_data()
-        self.neuralNet = MLPRegressor(learning_rate_init=0.01,
+        self.neuralNet = MLPRegressor(learning_rate_init=.01,
+                                      n_iter_no_change=2000,
                                       learning_rate='constant',
-                                      n_iter_no_change=1000,
+                                      activation='tanh',
                                       verbose=True,
+                                      alpha=0.05,
                                       random_state=1,
-                                      hidden_layer_sizes=150,
-                                      max_iter=100000)
-        self.neuralNet.fit(X[:, :-1], X[:, -1])
-        print(self.neuralNet.best_loss_)
+                                      hidden_layer_sizes=(50, 100, 50),
+                                      max_iter=30000)
+        self.neuralNet.fit(X, Y)
         pickle.dump(self.neuralNet, open(modelSaveFile, 'wb'))
         print(f"Training finished in {self.neuralNet.n_iter_} cycles.")
 
@@ -106,6 +111,50 @@ class AIClient:
 
         self.socketWrapper.send(actuators)
         
+    def tune_hyperparameters(self) -> None:
+        """
+        Used to find the best parameters for tuning the neural network.
+        """
+        network = MLPRegressor()
+        param_grid = {
+            'hidden_layer_sizes': [(50,100,50)],
+            'activation': ['relu','tanh','logistic'],
+            'learning_rate_init': [.01, .015, .02],
+            'alpha': [.01, .025, .05],
+            'learning_rate': ['constant','adaptive'],
+            'solver': ['adam'],
+            'max_iter': [30000],
+            'n_iter_no_change': [1000]
+        }
+
+        gsc = GridSearchCV(network,
+                           param_grid,
+                           cv=2, 
+                           scoring='neg_mean_squared_error', 
+                           verbose=1, 
+                           n_jobs=-1)
+        
+        grid_result = gsc.fit(X, Y)
+        best_params = grid_result.best_params_
+
+        best_mlp = MLPRegressor(hidden_layer_sizes = best_params["hidden_layer_sizes"], 
+                                activation = best_params["activation"],
+                                verbose = True,
+                                learning_rate = best_params['learning_rate'],
+                                alpha = best_params['alpha'],
+                                solver = best_params["solver"],
+                                max_iter = 30000,
+                                n_iter_no_change = 1000
+        )
+        scoring = {
+            'abs_error': 'neg_mean_absolute_error',
+            'squared_error': 'neg_mean_squared_error',
+            'r2':'r2'
+        }
+        scores = cross_validate(best_mlp, X, Y, cv=2, scoring=scoring, return_train_score=True, return_estimator = True)
+        print(best_params)
+        print(scores)
+    
     def plot_loss(self) -> None:
         """
         Plots the model loss over the number of iterations it has performed.
@@ -113,23 +162,25 @@ class AIClient:
         """
         try:
             with open(modelSaveFile, 'rb') as file:
-                self.neuralNet = pickle.load(file)
+                self.neuralNet : MLPRegressor = pickle.load(file)
         except Exception:
             self.train_network()
             with open(modelSaveFile, 'rb') as file:
-                self.neuralNet = pickle.load(file)
+                self.neuralNet : MLPRegressor = pickle.load(file)
             
         loss_values = self.neuralNet.loss_curve_
         best_loss = self.neuralNet.best_loss_
         best_loss_iteration = loss_values.index(best_loss)
+        r2_score = cross_val_score(self.neuralNet, X, Y, cv=5, scoring='r2')
+        print(r2_score)
         plt.figure("Loss per iteration")
+        plt.subplot(2, 1, 1)
         plt.plot(loss_values, label="Loss")
-        plt.plot(best_loss_iteration, best_loss, 'ro', label=f"Best loss ({round(best_loss, 2)})")
+        plt.plot(best_loss_iteration, best_loss, 'ro', label=f"Best loss: {round(best_loss, 2)}")
         plt.xlabel("Iterations")
         plt.ylabel("Loss")
         plt.legend()
         plt.show()
-
 
 class RLClient:
     def __init__(self) -> None:
