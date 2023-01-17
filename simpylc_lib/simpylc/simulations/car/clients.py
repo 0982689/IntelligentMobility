039a4e2cfae90a001657d7bd5316b4cdc2761339
Melroy Krebbekx <0982689@hr.nl>
2023-01-17 10:59:00 +0100

    test print

--- simpylc_lib/simpylc/simulations/car/clients.py
+++ simpylc_lib/simpylc/simulations/car/clients.py
@@ -170,4 +170,4 @@ class RLClient:
                 return
             print(steering_angle)
             self.servo.set_pulse(int(steering_angle))
-            time.sleep(.5)
+            time.sleep(.7)
